aa5fc5c2feb92cff153543914cf16da4ac111841
Stefano Huber <stefano.huber@gmail.com>
2018-11-28 14:25:29 +0100

    Edited code for stl priority queue, with code from cplusplus reference

--- snippets/datastructures/priority_queue.cpp
+++ snippets/datastructures/priority_queue.cpp
@@ -1,3 +1,17 @@
 
-priority_queue<int, vector<int>, greater<int> > pq;  // Extracts the smaller (min prior queue)
-priority_queue<int, vector<int> > pq;               // Extracts the bigger (max prior queue)
+class mycomparison{
+  bool reverse;
+public:
+  mycomparison(const bool& revparam=false)
+    {reverse=revparam;}
+  bool operator() (const pair<int, int> & lhs, const pair<int, int> &rhs) const{
+    // Custom comparison operator among two arbitrary objects, in this case a pair.
+    if (reverse) return (lhs.second>rhs.second);
+    else return (lhs.second<rhs.second);
+  }
+};
+
+typedef priority_queue<pair<int, int> ,vector<pair<int, int> >, mycomparison> mypq_type;
+
+mypq_type q1;       // Max priority queue (extracts in order 6, 4, 2)
+mypq_type q1(mycomparison(true));          // Min priority queue (extracts in order (2, 4, 6))
