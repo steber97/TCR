f8ddca4d9421224da7a01d46aa01026923596bf2
Stefano Huber <stefano.huber@gmail.com>
2018-11-28 13:54:43 +0100

    Added syntax for min and max priority queue

---
+++ snippets/datastructures/priority_queue.cpp
@@ -0,0 +1,3 @@
+
+priority_queue<int, vector<int>, greater<int> > pq;  // Extracts the smaller (min prior queue)
+priority_queue<int, vector<int> > pq;               // Extracts the bigger (max prior queue)
